faf452d700f40d2373821726aa6fdc374ef6ee89
Ishmael Kargbo <ishmaelkargbo5@outlook.com>
2025-05-23 06:10:28 +0000

    update the handlers to accept Command Interface

--- handler.go
+++ handler.go
@@ -2,10 +2,10 @@ package cqrs
 
 import "context"
 
-type CommandHandle[cmd Command] interface {
-	Handle(ctx context.Context, cmd Command) error
+type CommandHandle[T Command] interface {
+	Handle(ctx context.Context, cmd T) error
 }
 
-type QueryHandle[query Query, R any] interface {
-	Handle(ctx context.Context, query Query) (R, error)
+type QueryHandle[Q Query, R any] interface {
+	Handle(ctx context.Context, query Q) (R, error)
 }
