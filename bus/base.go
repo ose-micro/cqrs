a4013b4e37b944088962409153819aa5acfa5807
Ishmael Kargbo <ishmaelkargbo5@outlook.com>
2025-07-22 11:49:01 +0000

    close

--- bus/base.go
+++ bus/base.go
@@ -7,5 +7,6 @@ import (
 type Bus interface {
 	Publish(subject string, data any) error
 	Subscribe(subject, queue string, handler func(ctx context.Context, data any) error) error
+	Close() error
 }
 
--- bus/rabbit/type.go
+++
@@ -1,8 +0,0 @@
-package rabbit
-
-type Config struct {
-	URL              string `mapstructure:"url"`
-	Exchange         string `mapstructure:"exchange" default:"ose.exchange"`
-	ExchangeType     string `mapstructure:"exchange_type" default:"topic"`
-	ReconnectBackoff int    `mapstructure:"reconnect_backoff" default:"5"`
-}
